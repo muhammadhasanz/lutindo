9f3458a31e4cf4bf84def467d7b1a224c8154e54
unknown <mhmmdhasanz@gmail.com>
2022-01-12 07:31:52 +0700

    Updated Config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://www.lutindo.com/';
+$root = "https://" . $_SERVER['HTTP_HOST'];
+$config['base_url']    = $root;
+// $config['base_url'] = 'https://www.lutindo.com/';
 
 /*
 |--------------------------------------------------------------------------
